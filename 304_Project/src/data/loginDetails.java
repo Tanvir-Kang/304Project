dca48119fd71b71e990896cc53a098f518b014c3
Zachary Maludzinski <Zacharymaludzinski@gmail.com>
2018-11-17 18:12:56 -0800

    Fixed Login Details IMPORTANT - Untrack and ignore loginDetails.java

---
+++ 304_Project/src/data/loginDetails.java
@@ -0,0 +1,18 @@
+package data;
+
+public class loginDetails {
+	String uid = "zmaludzi";
+	String pw = "32950164";
+	String url = "jdbc:sqlserver://sql04.ok.ubc.ca:1433;DatabaseName=db_zmaludzi;";
+	
+	public String getUid() {
+		return uid;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public String getUrl() {
+		return url;
+	}
+	
+}
